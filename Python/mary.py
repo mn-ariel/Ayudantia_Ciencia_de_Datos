ba92eabca753cbbb5825683011196af7a7237ddf
MN Ariel <mn.ariel13@gmail.com>
2024-03-27 16:46:51 -0400

    end 2 intro python

--- Python/mary.py
+++
@@ -1,5 +0,0 @@
-# mary.py - Un programa sencillo
-print("Mary tenía un pequeño cordero")
-print("su lana era blanca como la nieve;")
-print("y a dondequiera que fuera Mary", end=" ")
-print("su cordero seguro la seguiría.")
